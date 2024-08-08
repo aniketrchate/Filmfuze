417ae58128bae332ed9ff4d77a299f049637f4a4
aniketrchate <anikrchate@gmail.com>
2024-08-09 01:16:11 +0530

    new file:   src/redux/type.js

---
+++ src/redux/type.js
@@ -0,0 +1,5 @@
+// src/redux/types.js
+export const ADD_MOVIE = 'ADD_MOVIE';
+export const REMOVE_MOVIE = 'REMOVE_MOVIE';
+export const FETCH_MOVIE_SUCCESS = 'FETCH_MOVIE_SUCCESS';
+export const FETCH_MOVIE_FAILURE = 'FETCH_MOVIE_FAILURE';
