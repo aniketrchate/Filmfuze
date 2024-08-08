4c23a5309e25bc93fcbc4e027eb6d5f56e46201c
aniketrchate <anikrchate@gmail.com>
2024-08-09 01:19:11 +0530

    created new file and added basic code src/redux/actions/movieActions.js

---
+++ src/redux/actions/movieActions.js
@@ -0,0 +1,30 @@
+// src/redux/actions/movieActions.js
+import axios from 'axios';
+import { ADD_MOVIE, REMOVE_MOVIE, FETCH_MOVIE_SUCCESS, FETCH_MOVIE_FAILURE } from '../types';
+
+const API_KEY = 'your_omdb_api_key'; // Replace with your OMDB API key
+
+export const addMovie = (movie) => ({
+    type: ADD_MOVIE,
+    payload: movie
+});
+
+export const removeMovie = (id) => ({
+    type: REMOVE_MOVIE,
+    payload: id
+});
+
+export const fetchMovie = (title) => async (dispatch) => {
+    try {
+        const response = await axios.get(`https://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`);
+        dispatch({
+            type: FETCH_MOVIE_SUCCESS,
+            payload: response.data
+        });
+    } catch (error) {
+        dispatch({
+            type: FETCH_MOVIE_FAILURE,
+            payload: error.message
+        });
+    }
+};
