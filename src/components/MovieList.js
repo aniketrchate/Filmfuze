8985e9b470926785297737e6bee2793b3b7673a2
aniketrchate <anikrchate@gmail.com>
2024-08-09 04:44:28 +0530

    Refactor MovieList component: updated CSS, converted to class component, and connected to Redux store

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,56 +1,70 @@
-// src/components/MovieList.js
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { removeMovie, selectMovie } from '../redux/actions/movieActions';
 import { Link } from 'react-router-dom';
 
-const MovieList = () => {
-    const dispatch = useDispatch();
-    const movies = useSelector(state => state.movies);
-
-    const handleRemove = (id) => {
-        dispatch(removeMovie(id));
+class MovieList extends Component {
+    handleRemoveMovie = (id) => {
+        this.props.removeMovie(id);
     };
 
-    const handleView = (movie) => {
-        dispatch(selectMovie(movie));
+    handleSelectMovie = (movie) => {
+        this.props.selectMovie(movie);
     };
 
-    return (
-        <div className="container mt-4">
-            <h2>My Movie List</h2>
-            <div className="row">
-                {movies.length > 0 ? (
-                    movies.map((movie) => (
-                        <div key={movie.imdbID} className="col-md-4 mb-4">
-                            <div className="card">
-                                <img src={movie.Poster} alt={movie.Title} className="card-img-top" />
-                                <div className="card-body">
-                                    <h5 className="card-title">{movie.Title}</h5>
-                                    <p className="card-text">{movie.Year}</p>
-                                    <button
-                                        className="btn btn-danger"
-                                        onClick={() => handleRemove(movie.imdbID)}
-                                    >
-                                        Remove
-                                    </button>
-                                    <Link
-                                        to={`/movie/${movie.imdbID}`}
-                                        className="btn btn-primary ml-2"
-                                        onClick={() => handleView(movie)}
-                                    >
-                                        View Details
-                                    </Link>
+    render() {
+        const { movies } = this.props;
+
+        return (
+            <div className="container mt-4">
+                <h2 className="mb-4">My Movie List</h2>
+                <div className="row">
+                    {movies.length > 0 ? (
+                        movies.map(movie => (
+                            <div key={movie.imdbID} className="col-md-3 mb-4">
+                                <div className="movie-card">
+                                    <img 
+                                        src={movie.Poster} 
+                                        alt={movie.Title} 
+                                        className="movie-img"
+                                    />
+                                    <div className="card-body">
+                                        <h5>{movie.Title} ({movie.Year})</h5>
+                                        <div className="d-flex justify-content-between">
+                                            <button 
+                                                className="btn btn-danger btn-sm"
+                                                onClick={() => this.handleRemoveMovie(movie.imdbID)}
+                                            >
+                                                Remove
+                                            </button>
+                                            <Link 
+                                                to={`/movie/${movie.imdbID}`} 
+                                                className="btn btn-info btn-sm"
+                                                onClick={() => this.handleSelectMovie(movie)}
+                                            >
+                                                View
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                ) : (
-                    <p>No movies in your list</p>
-                )}
+                        ))
+                    ) : (
+                        <p>No movies in the list.</p>
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    movies: state.movies
+});
+
+const mapDispatchToProps = {
+    removeMovie,
+    selectMovie
 };
 
-export default MovieList;
+export default connect(mapStateToProps, mapDispatchToProps)(MovieList);
