0fee8c6cc23b24fabf47d1f445a7aac889ef4efd
aniketrchate <anikrchate@gmail.com>
2024-08-09 02:34:25 +0530

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,11 @@
 // src/components/MovieList.js
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeMovie } from '../redux/actions/movieActions';
 
 const MovieList = () => {
-    const movies = useSelector(state => state.movies);
     const dispatch = useDispatch();
+    const movies = useSelector(state => state.movies);
 
     const handleRemove = (id) => {
         dispatch(removeMovie(id));
@@ -14,17 +14,25 @@ const MovieList = () => {
     return (
         <div className="container mt-4">
             <h2>My Movie List</h2>
-            <ul className="list-group">
+            <div className="row">
                 {movies.map(movie => (
-                    <li key={movie.imdbID} className="list-group-item d-flex justify-content-between align-items-center">
-                        <div>
-                            <h5>{movie.Title} ({movie.Year})</h5>
-                            <p>{movie.Plot}</p>
+                    <div className="col-md-4 mb-4" key={movie.imdbID}>
+                        <div className="card">
+                            <img src={movie.Poster} alt={movie.Title} className="card-img-top" />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.Title}</h5>
+                                <p className="card-text">{movie.Plot}</p>
+                                <button 
+                                    className="btn btn-danger"
+                                    onClick={() => handleRemove(movie.imdbID)}
+                                >
+                                    Remove
+                                </button>
+                            </div>
                         </div>
-                        <button className="btn btn-danger" onClick={() => handleRemove(movie.imdbID)}>Remove</button>
-                    </li>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 };
