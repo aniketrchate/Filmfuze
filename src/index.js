ca8e8468a7dd4a1fb571a6ce40e235cba0feda1a
aniketrchate <anikrchate@gmail.com>
2024-08-11 21:19:20 +0530

    Update ReactDOM import and rendering method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+// Create the root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the application
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
